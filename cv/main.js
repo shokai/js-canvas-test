6d831b9b2e146ad4b2b6a0248a4f1ac716eea456
Sho Hashimoto <hashimoto@shokai.org>
2011-08-08 15:53:23 +0900

    binarize

--- cv/main.js
+++ cv/main.js
@@ -1,44 +1,57 @@
 
+var canvas = null;
+var ctx = null;
+
 var make_btn = function(label){
     return $('<input>').addClass('btn').attr('type','button').attr('id', 'btn_'+label).attr('value', label);
 };
 
 $(function(){
     canvas_init();
-
-
     make_btn('reset').appendTo('#ctrls');
     $('#btn_reset').click(canvas_init);
     make_btn('grayscale').appendTo('#ctrls');
     $('#btn_grayscale').click(grayscale);
+    make_btn('binarize').appendTo('#ctrls');
+    $('#btn_binarize').click(binarize);
 });
 
 var canvas_init = function(){
-    var canvas = $('canvas#img');
-    var ctx = canvas[0].getContext('2d');
+    canvas = $('canvas#img');
+    ctx = canvas[0].getContext('2d');
     var img = new Image();
     img.onload = function(){
         canvas.attr('width', img.width).attr('height', img.height);
         ctx.drawImage(img, 0, 0, img.width, img.height);
     };
-    img.src = '../shokai.png';
+    img.src = '../shokai-big.jpg';
 };
 
 var grayscale = function(){
-    var canvas = $('canvas#img');
-    var ctx = canvas[0].getContext('2d');
     var img = ctx.getImageData(0, 0, canvas[0].width, canvas[0].height);
-    var data = img.data;
-    for(var i = 0; i < data.length; i+=4){
-        var r = data[i]&0xFF;
-        var g = data[i+1]&0xFF;
-        var b = data[i+2]&0xFF;
+    for(var i = 0; i < img.data.length; i+=4){
+        var r = img.data[i]&0xFF;
+        var g = img.data[i+1]&0xFF;
+        var b = img.data[i+2]&0xFF;
         var gray = (r+g+b)/3;
-        data[i] = gray;
-        data[i+1] = gray;
-        data[i+2] = gray;
+        img.data[i] = gray;
+        img.data[i+1] = gray;
+        img.data[i+2] = gray;
     }
-    img.data = data;
     ctx.putImageData(img, 0, 0);
 };
 
+var binarize = function(){
+    var img = ctx.getImageData(0, 0, canvas[0].width, canvas[0].height);
+    for(var i = 0; i < img.data.length; i+=4){
+        var r = img.data[i]&0xFF;
+        var g = img.data[i+1]&0xFF;
+        var b = img.data[i+2]&0xFF;
+        var bin = 0;
+        if((r+g+b)/3 > 128) bin = 255;
+        img.data[i] = bin;
+        img.data[i+1] = bin;
+        img.data[i+2] = bin;
+    }
+    ctx.putImageData(img, 0, 0);
+}
